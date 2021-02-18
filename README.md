fc4d591f2a0a88aebbd2eb80ce84d707ce68cde8
Luick Klippel <luickklippel@Luicks-MacBook-Pro.local>
2021-02-18 23:17:31 +0100

    improving source code and updating readme.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Simulation of simple state information transmission by quantum state collapse
+
+This repository contains the summarized code of [this](https://dataespresso.com/en/2018/07/29/Tutorial-Getting-started-with-Quantum-Teleportation-Python/) tutorial about "quantum teleportation".
+The code is not copied but instead improved!
+The improvements done are meant to enhance the understandability and include the complete overhaul of the code layout as well as the code commenting!
--- main.py
+++ main.py
@@ -4,90 +4,97 @@ from projectq import MainEngine
 def main():
     quantum_engine = MainEngine()
 
-    #The bit we want to send
-    bit = 1
+    # information we want to transmit
+    information_to_transmit = 1
 
-    # make a Bell-pair
-    qubit_one, qubit_two = create_bell_pair(quantum_engine)
+    # this qubit is the senders qubit
+    qubit_one = quantum_engine.allocate_qubit()
 
-    # Entangle the bit with qubit_one
-    print('Sending bit: ',bit)
-    classical_encoded_message = create_message(quantum_engine=quantum_engine,qubit_one=qubit_one, message_value = bit)
-    print('Encoded in classical message: ', classical_encoded_message)
+    # the receivers qubit
+    qubit_two = quantum_engine.allocate_qubit()
+
+    # create_bell_pair is creating a bell-pair
+    # a bell pair resembles two qubits two entangled by a CNOT gate
+    # thereby the input qubit is set to a superposition (done by the Hadamard gate)
+    # This results in two qubits with a correlatable value (if additional information is given)
+    # the additional information in this case is the classical_encoded_message variable which
+    # holds data about the  first measurement from the sender
+    qubit_one, qubit_two = create_bell_pair(quantum_engine, qubit_one, qubit_two)
+
+    print('Information that is transmitted: ',information_to_transmit)
+
+    # preparing the senders qubits and collapsing their state(measuring them) so that information can be send to the receiver
+    classical_encoded_message = init_sender_qubits(quantum_engine=quantum_engine, qubit_one=qubit_one, message_value = information_to_transmit)
+    print('Prepared and measured qubits in classical message: ', classical_encoded_message)
 
     # Sending the message along with the second qubit for state re-creation
-    recieved_bit = message_reciever(quantum_engine, classical_encoded_message,qubit_two)
+    recieved_bit = read_receiver_qubit(quantum_engine, classical_encoded_message,qubit_two)
 
     print('Received bit: ', recieved_bit)
 
-def create_bell_pair(quantum_engine):
-    # Qubit one is 'our' qubit, and will be used to create a message
-    qubit_one = quantum_engine.allocate_qubit()
-    # Qubit two is 'the receivers' qubit, and will be used to re-create the message state
-    qubit_two = quantum_engine.allocate_qubit()
-    '''
-    Hadamard gate to put Qubit one in superposition
-    This sets the value of a equal probability of being 1 or 0
-    '''
+
+def create_bell_pair(quantum_engine, qubit_one='', qubit_two=''):
+    # Hadamard gate to put Qubit one in superposition
+    # This sets the value of a equal probability of being 1 or 0
     H | qubit_one
 
-    '''
-    CNOT gate to flip the second Qubit conditionally
-    on the first qubit being in the state |1⟩
-    '''
+
+    # CNOT gate to flip the second Qubit conditionally
+    # on the first qubit being in the state |1⟩
     CNOT | (qubit_one, qubit_two)
 
     return qubit_one, qubit_two
 
 
-def create_message(quantum_engine='',qubit_one='', message_value = 0):
+def init_sender_qubits(quantum_engine='',qubit_one='', message_value = 0):
+    # this qubit "encodes" the information we want to transmit(message_value)
+    qubit_encoding_information = quantum_engine.allocate_qubit()
 
-    qubit_to_send = quantum_engine.allocate_qubit()
+    # if the message_value is (bianry) 1 the qubits qubit_encoding_information state is flipped with a X-Gate
     if message_value == 1:
-        '''
-        setting the qubit to positive if message_value is 1
-        by flipping the base state with a Pauli-X gate.
-        '''
-        X | qubit_to_send
+        X | qubit_encoding_information
 
 
-    # entangle the original qubit with the message qubit
-    CNOT | (qubit_to_send, qubit_one)
+    # entangling the senders(qubit_one) qubit with the qubit that encodes the inforamtion(qubit_encoding_information) we want to transmit
+    CNOT | (qubit_encoding_information, qubit_one)
 
-    '''
-    1 - Put the message qubit in superposition
-    2 - Measure out the two values to get the classical bit value
-        by collapsing the state.
-    '''
-    H | qubit_to_send
-    Measure | qubit_to_send
+
+    # the message qubit is set to superposition (by applying the Hadamard-Gate)
+    H | qubit_encoding_information
+
+    # both, the information encoding qubit(qubit_encoding_information) and the qubits are measured
+    # the resulting effect is that the senders qubits (qubit_one) state may change (thorugh the CNOT-Gate applied above)
+    Measure | qubit_encoding_information
     Measure | qubit_one
 
-    # The qubits are now turned into normal bits we can send through classical channels
-    classical_encoded_message = [int(qubit_to_send), int(qubit_one)]
+    # since both qubits are measured they now have definit and readable values which are sent to the receiver (thourgh classical channels)
+    classical_encoded_message = [int(qubit_encoding_information), int(qubit_one)]
 
     return classical_encoded_message
 
 
-def message_reciever(quantum_engine,message,qubit_two):
-    '''
-    Pauli-X and/or Pauli-Z gates are applied to the Qubit,
-    conditionally on the values in the message.
-    '''
+def read_receiver_qubit(quantum_engine,message,qubit_two):
+
+    # if the measured qubit, qubit_encoding_information has value 1 a X-Gate is applied to the receivers qubit(qubit_two)
     if message[1] == 1:
         X | qubit_two
+    # if the measured sender qubit(qubit_one) has value 1 a Z-Gate is applied to the receivers qubit(qubit_two)
     if message[0] == 1:
         Z | qubit_two
 
-    '''
-    Measuring the Qubit and collapsing the state down to either 1 or 0
-    '''
+    # finally the receivers qubit is measured
+    # the result of the measurement is dependent on manipulation above
+    # without the manipulation, the result of the receivers qubit measurement would be useless
+    # the measurement now is not just dependent on the bell-pair-link(entanglement)
+    # (between sender and receiver qubits) which would still not be enough to transmit information
+    # but also completed by the information given by the classical channel which completes the transmission process
     Measure | qubit_two
 
     quantum_engine.flush()
 
-    recieved_bit = int(qubit_two)
-    return recieved_bit
+    recieved_information = int(qubit_two)
+
+    return recieved_information
 
 if __name__ == "__main__":
     main()
